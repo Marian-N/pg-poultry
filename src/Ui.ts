06f70c9835f0839bba98ccad7868483716099c9d
Marian Nisky <mariannisky@gmail.com>
2022-12-26 17:51:11 +0100

    refactor popup

--- src/Ui.ts
+++ src/Ui.ts
@@ -1,7 +1,40 @@
 import Pointer from './Pointer';
 
+class Popup {
+  public element: HTMLElement;
+  public closeButton: HTMLElement;
+  public title: HTMLElement;
+  public content: HTMLElement;
+
+  constructor() {
+    this.init();
+    this.closeButton.addEventListener('click', () => {
+      this.element.classList.remove('active');
+    });
+  }
+
+  private init() {
+    const popup = document.getElementById('popup') as HTMLElement;
+    const closeButton = popup?.getElementsByClassName(
+      'popup__header__close'
+    )[0] as HTMLElement;
+    const popupTitle = popup?.getElementsByClassName(
+      'popup__header__title'
+    )[0] as HTMLElement;
+    const popupContent = popup?.getElementsByClassName(
+      'popup__body__content'
+    )[0] as HTMLElement;
+
+    this.element = popup;
+    this.closeButton = closeButton;
+    this.title = popupTitle;
+    this.content = popupContent;
+  }
+}
+
 class Ui {
   private pointer: Pointer;
+  public popup: Popup;
 
   constructor(pointer: Pointer) {
     this.pointer = pointer;
@@ -13,13 +46,7 @@ class Ui {
   }
 
   private initPopup() {
-    const popup = document.getElementById('popup') as HTMLElement;
-    const closeButton = popup?.getElementsByClassName(
-      'popup__header__close'
-    )[0];
-    closeButton?.addEventListener('click', () => {
-      popup.classList.remove('active');
-    });
+    this.popup = new Popup();
     this.pointer.onClick = (entity, event) => {
       entity.handleClick(event);
     };
